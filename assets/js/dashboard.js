6eaa169951b5026c4edd6f304784cc2574dcde74
Yusron alamsyah <icus.yusron@gmail.com>
2024-10-22 15:26:34 +0700

    fix endpoint : getData pegadaian

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,7 +1,7 @@
-function loading(flag){
-    if(flag){
+function loading(flag) {
+    if (flag) {
         $('#loading').show();
-    }else{
+    } else {
         $('#loading').hide();
     }
 }
@@ -11,53 +11,104 @@ function getUrlParameter(name) {
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
     var results = regex.exec(window.location.href);
     return results === null
-      ? ""
-      : decodeURIComponent(results[1].replace(/\+/g, " "));
+        ? ""
+        : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function redirectPage(url){
+function redirectPage(url) {
     var getNIK = getUrlParameter("nik");
     var getPhone = getUrlParameter("phone");
     var getName = getUrlParameter("name");
-    if(getNIK){
-        window.location.href = url+"?nik="+getNIK;
+    if (getNIK) {
+        window.location.href = url + "?nik=" + getNIK;
         return
-    }else if(getPhone){
-        window.location.href = url+"?phone="+getPhone;
+    } else if (getPhone) {
+        window.location.href = url + "?phone=" + getPhone;
         return
-    }else if(getName){
-        window.location.href = url+"?name="+getName;
+    } else if (getName) {
+        window.location.href = url + "?name=" + getName;
         return
-    }else{
+    } else {
         window.location.href = url;
     }
 }
 
 function calculateAge(birthdate) {
-    var birthDate = new Date(birthdate); 
-    var today = new Date(); 
+    var birthDate = new Date(birthdate);
+    var today = new Date();
     var age = today.getFullYear() - birthDate.getFullYear();
     var monthDifference = today.getMonth() - birthDate.getMonth();
-    
+
     if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
-        age--; 
+        age--;
     }
 
     return age;
 }
 
-var getNIK = getUrlParameter("nik");
-if(getNIK){
-    $("#input-nik").val(getNIK);
+function joinStringArray(str, glue) {
+    try {
+        const data = JSON.parse(str);
+        return data.join(glue);
+    } catch (e) {
+        return '';
+    }
+};
+
+function formatRupiah(number) {
+    if (number == null || number === '') {
+        return 'Rp. 0';
+    }
+
+    var reverse = number.toString().split('').reverse().join('');
+
+    var rupiah = reverse.match(/\d{1,3}/g).join('.');
+
+    return 'Rp. ' + rupiah.split('').reverse().join('');
 }
-var getPhone = getUrlParameter("phone");
-if(getPhone){
-    $("#input-nohp").val(getPhone);
+
+function isCampaignRunning(start_date, end_date) {
+    return (
+        !end_date ||
+        (dateTime(start_date) < dateTime() &&
+            dateTime(end_date) > dateTime())
+    );
+};
+
+function formatDate(dateString) {
+    dateString = dateString.replace(' ', 'T');
+
+    var date = new Date(dateString);
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    return day + '-' + month + '-' + year;
 }
-var getName = getUrlParameter("name");
-if(getName){
-    $("#input-nama").val(getName);
+
+function toEmptyString(value) {
+    return value == null ? '' : value; 
 }
+
+// var getNIK = getUrlParameter("nik");
+// if(getNIK){
+//     $("#input-nik").val(getNIK);
+// }
+// var getPhone = getUrlParameter("phone");
+// if(getPhone){
+//     $("#input-nohp").val(getPhone);
+// }
+// var getName = getUrlParameter("name");
+// if(getName){
+//     $("#input-nama").val(getName);
+// }
 
 $(document).ready(function () {
     var sessionUser = Cookies.get('sessionUser');
@@ -65,22 +116,51 @@ $(document).ready(function () {
     if (!sessionUser && !username) {
         window.location.href = "login.html";
     }
-    if(sessionUser){
+    if (sessionUser) {
         $('#tag-username').text(sessionUser)
     }
-    
-    if(username){
+
+    if (username) {
         $('#tag-username').text(username)
     }
     $('#logoutButton').click(function () {
 
         Cookies.remove('sessionUser');
-        
+
         localStorage.removeItem("isLoggedIn");
         localStorage.removeItem("username");
         localStorage.removeItem("loginTime");
 
         window.location.href = "login.html";
-        alert("logout");
+    });
+
+    $('#input-nik').keydown(function(event) {
+        if (event.keyCode === 13) { 
+            event.preventDefault(); 
+            var nik = $(this).val();
+            $("#input-nohp").val("");
+            $("#input-nama").val("");
+            alert("Search : "+nik);
+        }
+    });
+
+    $('#input-nohp').keydown(function(event) {
+        if (event.keyCode === 13) { 
+            event.preventDefault(); 
+            var nohp = $(this).val();
+            $("#input-nik").val("");
+            $("#input-nama").val("");
+            alert("Search : "+nohp);
+        }
+    });
+
+    $('#input-nama').keydown(function(event) {
+        if (event.keyCode === 13) { 
+            event.preventDefault(); 
+            var nama = $(this).val();
+            $("#input-nohp").val("");
+            $("#input-nik").val("");
+            alert("Search : "+nama);
+        }
     });
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    
-    $('#formLogin').submit(function(event){
-        event.preventDefault(); 
+$(document).ready(function () {
+
+    $('#formLogin').submit(function (event) {
+        event.preventDefault();
         // Get the form data
         var formData = {
             identifier: $('input[name="userId"]').val(),
@@ -14,7 +14,7 @@ $(document).ready(function(){
             url: 'https://staging-qore-data-whale-474358.qore.dev/v1/authorize',
             contentType: 'application/json',
             data: JSON.stringify(formData),
-            success: function(response) {
+            success: function (response) {
                 $('#loading').hide();
 
                 Cookies.set('sessionUser', formData.identifier, { expires: 1 });
@@ -25,55 +25,55 @@ $(document).ready(function(){
 
                 window.location.href = "outlet.html";
             },
-            error: function() {
+            error: function () {
                 $('#loading').hide();
                 alert('Mohon maaf username atau password yang Anda masukkan salah');
             }
         });
     });
 
-    $('#formOutlet').submit(function(event){
-        event.preventDefault(); 
+    $('#formOutlet').submit(function (event) {
+        event.preventDefault();
 
         // Get the form data
         var nik = $('#nik').val().trim();
         var phone = $('#phone').val().trim();
         var fullname = $('#fullname').val().trim();
-        
+
         if (!nik && !fullname && !phone) {
             alert('Minimal harus terisi NIK, atau No. HP dan Nama Lengkap.');
             return;
         }
-
-        if(nik){
-            window.location.href = "dashboard.html?nik="+nik;
+        var params
+        if (nik) {
+            params = "?nik=" + nik
+        }
+        if (phone) {
+            params = "?phone=" + phone
+        }
+        if (fullname) {
+            params = "?name=" + fullname
         }
 
-        if(phone){
-            window.location.href = "dashboard.html?phone="+phone;
-        }
 
-        if(fullname){
-            window.location.href = "dashboard.html?name="+fullname;
-        }
-        
-
-
-        // $.ajax({
-        //     type: 'GET',
-        //     url: 'https://customer360.pegadaian.co.id/api/audience_detail?id='+nik,
-        //     contentType: 'application/json',
-        //     data: JSON.stringify({}),
-        //     success: function(response) {
-        //         alert('sukses');
-        //         window.location.href = "dashboard.html";
-        //     },
-        //     error: function() {
-        //         alert('Mohon maaf data tidak ditemukan');
-        //     }
-        // });
-        
-    
+        $('#loading').show();
+        $.ajax({
+            type: 'GET',
+            url: 'https://asia-southeast2-accenture-283510.cloudfunctions.net/pegadaian-customer360' + params,
+            contentType: 'application/json',
+            success: function (response) {
+                $('#loading').hide();
+                if (nik) {
+                    window.location.href = "data-nasabah.html?nik=" + nik;
+                } else {
+                    window.location.href = "data-nasabah.html?nik=" + response.data.audiences[0].nik
+                }
+            },
+            error: function () {
+                $('#loading').hide();
+                alert('Mohon maaf data tidak ditemukan');
+            }
+        });
 
 
 
