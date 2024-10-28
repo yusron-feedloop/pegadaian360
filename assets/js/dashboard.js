46a96105e10cc08b8ee77ea4680f734b56f1d0ff
Yusron alamsyah <icus.yusron@gmail.com>
2024-10-28 13:51:03 +0700

    fix riwayat transaksi & complain dummy

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -104,6 +104,33 @@ function formatDate(dateString) {
     return day + '-' + month + '-' + year;
 }
 
+function formatDateTime(dateString) {
+    dateString = dateString.replace(' ', 'T');
+
+    var date = new Date(dateString);
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    return day + '-' + month + '-' + year+' '+hours + ':' + minutes;
+}
+
 function getTime(dateString) {
     dateString = dateString.replace(' ', 'T');
 
